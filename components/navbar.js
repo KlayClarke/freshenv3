98f4afcabda8f589c70da1e52cbf5672538656ab
Klay Clarke <clarkeklay@gmail.com>
2022-04-29 16:57:05 -0400

    make navbar responsive, allow hamburger nav open and close using a "isNavOpen" state

--- components/navbar.js
+++ components/navbar.js
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 export default function Navbar() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
   return (
     <nav class="flex items-center justify-between flex-wrap bg-white border-b border-gray-300 p-6 top-0 left-0 right-0 sticky">
       <div class="flex items-center flex-shrink-0 text-blue-500 mr-6">
@@ -7,7 +10,10 @@ export default function Navbar() {
         </a>
       </div>
       <div class="block lg:hidden">
-        <button class="flex items-center px-3 py-2 border rounded bg-blue-500 text-white border-white  hover:border-blue-600">
+        <button
+          onClick={() => setIsNavOpen(!isNavOpen)}
+          class="flex items-center px-3 py-2 border rounded bg-blue-500 text-white border-white  hover:border-blue-600"
+        >
           <svg
             class="fill-current h-3 w-3"
             viewBox="0 0 20 20"
@@ -18,22 +24,28 @@ export default function Navbar() {
           </svg>
         </button>
       </div>
-      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div
+        class={
+          !isNavOpen
+            ? "hidden"
+            : "w-full block flex-grow lg:flex lg:items-center lg:w-auto"
+        }
+      >
         <div class="text-sm lg:flex-grow">
           <a
-            href="#responsive-header"
+            href="/explore"
             class="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-blue-500 mr-4"
           >
             Explore
           </a>
           <a
-            href="#responsive-header"
+            href="/login"
             class="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-blue-500 mr-4"
           >
             Login
           </a>
           <a
-            href="#responsive-header"
+            href="/join"
             class="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-blue-500"
           >
             Join
