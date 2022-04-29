84e9812de189c76a087cc6473ff2335432a95e9e
Klay Clarke <clarkeklay@gmail.com>
2022-04-29 17:38:34 -0400

    create login form

---
+++ pages/login.js
@@ -0,0 +1,43 @@
+export default function Login() {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <form className="bg-white shadow-sm border rounded-lg px-8 pt-6 pb-8 mb-4">
+        <div className="form-section">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Username
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500 focus:border-opacity-50"
+            id="username"
+            type={"text"}
+          />
+        </div>
+        <div className="form-section">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500 focus:border-opacity-50"
+            id="password"
+            type={"password"}
+          />
+        </div>
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 border border-blue-500 rounded w-full">
+          Join
+        </button>
+        <p className="mt-4">
+          Don't have an account?{" "}
+          <a href="/join" className="text-blue-500">
+            Join
+          </a>{" "}
+        </p>
+      </form>
+    </div>
+  );
+}
