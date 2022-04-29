b94947c20586a017fd097995bf42b02e2d2c9a46
Klay Clarke <clarkeklay@gmail.com>
2022-04-29 19:48:59 -0400

    declare min-h on main and max-w on container div that children components will populate (trying to not have to declare dimensions in content containers for each page)

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,9 @@ export default function Layout({ children }) {
   return (
     <>
       <Navbar />
-      <main>{children}</main>
+      <main className="min-h-screen flex flex-col items-center justify-center">
+        <div className="max-w-[1024px]">{children}</div>
+      </main>
       <Footer />
     </>
   );
