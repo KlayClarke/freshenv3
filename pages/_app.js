ac467f88137baa8933c6b0289b6595a73e7a2ca4
Klay Clarke <clarkeklay@gmail.com>
2022-04-30 15:40:16 -0400

    fontawesome config

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
-import Layout from "../components/layout";
-import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
+import "../styles/globals.css";
+
+import Layout from "../components/layout";
 import AuthWrapper from "../components/AuthWrapper";
 
 function MyApp({ Component, pageProps }) {
