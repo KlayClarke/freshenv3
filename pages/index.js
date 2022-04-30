040fb929744f6a0781712d656d9ece0ad2bdf7f6
Klay Clarke <clarkeklay@gmail.com>
2022-04-29 23:10:49 -0400

    render mapbox map on index page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
+import { useState, useEffect } from "react";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Script from "next/script";
+import mapboxgl from "mapbox-gl";
 
-export default function Home() {
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
+
+export default function Home(props) {
+  const [pageIsMounted, setPageIsMounted] = useState(false);
+
+  useEffect(() => {
+    setPageIsMounted(true);
+    console.log(process.env.NEXT_PUBLIC_MAPBOX_TOKEN);
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+    });
+  }, []);
+
   return (
     <div className="flex flex-col justify-center items-center h-screen gap-10">
+      <Head>
+        <Script src="https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.js" />
+      </Head>
       <h1 className="text-4xl max-w-4xl text-center text-blue-500">
         Whether you are a customer searching for a new hairstylist or a
         hairstylist searching for new customers,{" "}
@@ -24,6 +41,7 @@ export default function Home() {
           Log In Using Existing Account
         </a>
       </div>
+      <div id="map" className="w-[1000px] h-[400px]"></div>
     </div>
   );
 }
