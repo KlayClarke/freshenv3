ef096dee04707931e7d519cd5bd51c1a53c95ccf
Klay Clarke <clarkeklay@gmail.com>
2022-04-30 09:47:18 -0400

    initialize and render map on explore detail pages -- map centered and marked at current salon coordinates

---
+++ map/initializeDetailMap.js
@@ -0,0 +1,13 @@
+export default function initializeDetailMap(mapboxgl, map, salon) {
+  map.addControl(new mapboxgl.FullscreenControl());
+  const marker1 = new mapboxgl.Marker()
+    .setLngLat(salon.geometry.coordinates)
+    .setPopup(
+      new mapboxgl.Popup({ offset: 25 }).setHTML(`<h2>${salon.name}</h2>`)
+    )
+    .addTo(map);
+
+  map.on("style.load", () => {
+    map.addControl(new mapboxgl.NavigationControl());
+  });
+}
--- pages/explore/detail/[id].js
+++ pages/explore/detail/[id].js
@@ -1,12 +1,40 @@
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import mapboxgl from "mapbox-gl";
+import initializeDetailMap from "../../../map/initializeDetailMap";
 import clientPromise from "../../../lib/mongodb";
 
 // todo: render mapbox map on explore detail page
 
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
+
 export default function Detail({ salon }) {
+  const [pageIsMounted, setPageIsMounted] = useState(false);
+  const [Map, setMap] = useState();
+
+  useEffect(() => {
+    setPageIsMounted(true);
+
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      zoom: 16,
+      center: salon.geometry.coordinates,
+    });
+
+    setMap(map);
+  }, []);
+
+  useEffect(() => {
+    if (pageIsMounted && salon) {
+      initializeDetailMap(mapboxgl, Map, salon);
+    }
+  }, [pageIsMounted, salon, Map]);
+
   return (
     <div>
       <h1 className="text-4xl">{salon.name}</h1>
+      <div id="map" className="w-[1000px] h-[400px]"></div>
     </div>
   );
 }
