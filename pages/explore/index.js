83e566582ae3e6399014b633e0f10f0e0b9d9435
Klay Clarke <clarkeklay@gmail.com>
2022-04-29 20:31:10 -0400

    complete responsive styling for explore index page

--- pages/explore/index.js
+++ pages/explore/index.js
@@ -1,12 +1,12 @@
 import clientPromise from "../../lib/mongodb";
 
-export default function Explore({ salons, marginTop }) {
+export default function Explore({ salons }) {
   return (
-    <div className="mt-10 mb-10">
-      <div className="w-[100%] grid lg:grid-cols-2 gap-10 p-10">
+    <div className="mt-1 mb-1">
+      <div className="w-[100%] grid lg:grid-cols-2 gap-5 p-10">
         {salons.map((salon, index) => {
           return (
-            <div class="bg-white rounded-lg shadow border overflow-hidden">
+            <div class="bg-white rounded-lg shadow border border-gray-300 overflow-hidden">
               <div class="md:flex">
                 <div class="md:shrink-0">
                   <img
@@ -15,7 +15,7 @@ export default function Explore({ salons, marginTop }) {
                     alt="shop"
                   />
                 </div>
-                <div class="p-8 w-full">
+                <div class="p-8 min-w-[50%]">
                   <div class="uppercase tracking-wide text-sm text-blue-500 font-semibold">
                     {salon.type}
                   </div>
@@ -25,7 +25,7 @@ export default function Explore({ salons, marginTop }) {
                   >
                     {salon.name}
                   </a>
-                  <p class="mt-2 text-slate-500">
+                  <p class="mt-2 text-slate-500 h-fit min-w-[80%] truncate">
                     {salon.street_address} {salon.city}, {salon.state}{" "}
                     {salon.zip_code}
                   </p>
