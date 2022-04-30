af0a2d9fed63f77e98a957ee9c6408d57a1b0205
Klay Clarke <clarkeklay@gmail.com>
2022-04-29 21:20:39 -0400

    allow user to sort explore page by name, type and average price

--- pages/explore/index.js
+++ pages/explore/index.js
@@ -1,31 +1,73 @@
+import { useState, useEffect } from "react";
 import clientPromise from "../../lib/mongodb";
 
-export default function Explore({ salons }) {
+export default function Explore({
+  salonsByName,
+  salonsByType,
+  salonsByAveragePrice,
+}) {
+  const [sortBy, setSortBy] = useState("name");
+  const [salons, setSalons] = useState(salonsByName);
+
+  useEffect(() => {
+    if (sortBy === "name") {
+      setSalons(salonsByName);
+    } else if (sortBy === "type") {
+      setSalons(salonsByType);
+    } else if (sortBy === "average_price") {
+      setSalons(salonsByAveragePrice);
+    }
+  }, [sortBy]);
+
   return (
     <div className="mt-1 mb-1">
+      <div className="px-10 pt-10">
+        <div className="relative w-64">
+          <select
+            onChange={(e) => setSortBy(e.target.value)}
+            className="block appearance-none w-full bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+          >
+            <option value={"name"}>Name</option>
+            <option value={"type"}>Type</option>
+            <option value={"average_price"}>Average Price</option>
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg
+              className="fill-current h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+            </svg>
+          </div>
+        </div>
+      </div>
       <div className="w-[100%] grid lg:grid-cols-2 gap-5 p-10">
         {salons.map((salon, index) => {
           return (
-            <div class="bg-white rounded-lg shadow-sm border border-gray-300 overflow-hidden">
-              <div class="md:flex">
-                <div class="md:shrink-0">
+            <div
+              className="bg-white rounded-lg shadow-sm border border-gray-300 overflow-hidden"
+              key={index}
+            >
+              <div className="md:flex">
+                <div className="md:shrink-0">
                   <img
-                    class="h-48 w-full object-cover md:w-48"
+                    className="h-48 w-full object-cover md:w-48"
                     src={salon.image}
                     alt="shop"
                   />
                 </div>
-                <div class="p-8 min-w-[50%]">
-                  <div class="uppercase tracking-wide text-sm text-blue-500 font-semibold">
+                <div className="p-8 min-w-[50%]">
+                  <div className="uppercase tracking-wide text-sm text-blue-500 font-semibold">
                     {salon.type}
                   </div>
                   <a
                     href={`/explore/${salon._id}`}
-                    class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+                    className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
                   >
                     {salon.name}
                   </a>
-                  <p class="mt-2 text-slate-500 h-fit min-w-[80%] truncate">
+                  <p className="mt-2 text-slate-500 h-fit min-w-[80%] truncate">
                     {salon.street_address} {salon.city}, {salon.state}{" "}
                     {salon.zip_code}
                   </p>
@@ -44,10 +86,28 @@ export async function getServerSideProps() {
 
   const db = client.db("FreshenDatabase");
 
-  let salons = await db.collection("salons").find({}).toArray();
-  salons = JSON.parse(JSON.stringify(salons));
+  let salonsByName = await db
+    .collection("salons")
+    .find({})
+    .sort({ name: 1 })
+    .toArray();
+  salonsByName = JSON.parse(JSON.stringify(salonsByName));
+
+  let salonsByType = await db
+    .collection("salons")
+    .find({})
+    .sort({ type: 1 })
+    .toArray();
+  salonsByType = JSON.parse(JSON.stringify(salonsByType));
+
+  let salonsByAveragePrice = await db
+    .collection("salons")
+    .find({})
+    .sort({ average_price: 1 })
+    .toArray();
+  salonsByAveragePrice = JSON.parse(JSON.stringify(salonsByAveragePrice));
 
   return {
-    props: { salons },
+    props: { salonsByName, salonsByType, salonsByAveragePrice },
   };
 }
