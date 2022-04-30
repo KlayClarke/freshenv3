0b4d2653cc20bcf95cbad6cf25b0eff29ec57037
Klay Clarke <clarkeklay@gmail.com>
2022-04-29 20:31:24 -0400

    increase max-width from 1024px to 1200px

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
     <>
       <Navbar />
       <main className="min-h-screen flex flex-col items-center justify-center">
-        <div className="max-w-[1024px]">{children}</div>
+        <div className="max-w-[1200px]">{children}</div>
       </main>
       <Footer />
     </>
