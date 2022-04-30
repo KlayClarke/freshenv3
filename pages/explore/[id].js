6a8d4d3036542021f411a9e30933aacaf432ee30
Klay Clarke <clarkeklay@gmail.com>
2022-04-29 23:12:25 -0400

    pass specific shop to explore/[shopid]

---
+++ pages/explore/[id].js
@@ -0,0 +1,30 @@
+import { useRouter } from "next/router";
+import clientPromise from "../../lib/mongodb";
+
+export default function Detail({ salon }) {
+  return (
+    <div>
+      <h1 className="text-4xl">{salon.name}</h1>
+    </div>
+  );
+}
+
+export async function getServerSideProps({ query }) {
+  const client = await clientPromise;
+
+  const db = client.db("FreshenDatabase");
+
+  let salons = await db.collection("salons").find({}).toArray();
+
+  salons = JSON.parse(JSON.stringify(salons));
+
+  let salon;
+
+  for (let i = 0; i < salons.length; i++) {
+    salons[i]._id == query.id ? (salon = salons[i]) : "";
+  }
+
+  return {
+    props: { salon },
+  };
+}
