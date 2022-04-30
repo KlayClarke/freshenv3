04e1d703fea5e4b75eaced3630d4e72ce1198a54
Klay Clarke <clarkeklay@gmail.com>
2022-04-29 23:12:46 -0400

    revise sort by select input text

--- pages/explore/index.js
+++ pages/explore/index.js
@@ -27,9 +27,9 @@ export default function Explore({
             onChange={(e) => setSortBy(e.target.value)}
             className="block appearance-none w-full bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
           >
-            <option value={"name"}>Name</option>
-            <option value={"type"}>Type</option>
-            <option value={"average_price"}>Average Price</option>
+            <option value={"name"}>Sort by name</option>
+            <option value={"type"}>Sort by type</option>
+            <option value={"average_price"}>Sort by $</option>
           </select>
           <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
             <svg
