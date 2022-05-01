e3e7a475fed130026455d40b06deabc4d5d75db2
Klay Clarke <clarkeklay@gmail.com>
2022-05-01 16:43:30 -0400

    remove extra space at bottom of index/home

--- pages/index.js
+++ pages/index.js
@@ -39,33 +39,37 @@ export default function Home() {
   }, [pageIsMounted, data, Map]);
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen gap-10">
-      <Head>
-        <Script src="https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.js" />
-      </Head>
-      <h1 className="text-4xl max-w-4xl text-center text-blue-500">
-        Whether you are a customer searching for a new hairstylist or a
-        hairstylist searching for new customers,{" "}
-        <span className="font-bold">freshen</span> is the answer
-      </h1>
-      <div className="flex gap-5">
-        <a
-          href="/auth/join"
-          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 border border-blue-500 rounded"
-        >
-          Create New Account
-        </a>
-        <a
-          href="/auth/login"
-          className="bg-white hover:bg-gray-50 text-blue-500 font-bold py-2 px-4 border border-blue-500 rounded"
-        >
-          Log In Using Existing Account
-        </a>
+    <div className="flex flex-col justify-center items-center">
+      <div className="w-full xl:max-w-[1600px]">
+        <div className="flex flex-col justify-center items-center gap-10">
+          <Head>
+            <Script src="https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.js" />
+          </Head>
+          <h1 className="text-4xl max-w-4xl text-center text-blue-500">
+            Whether you are a customer searching for a new hairstylist or a
+            hairstylist searching for new customers,{" "}
+            <span className="font-bold">freshen</span> is the answer
+          </h1>
+          <div className="flex gap-5">
+            <a
+              href="/auth/join"
+              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 border border-blue-500 rounded"
+            >
+              Create New Account
+            </a>
+            <a
+              href="/auth/login"
+              className="bg-white hover:bg-gray-50 text-blue-500 font-bold py-2 px-4 border border-blue-500 rounded"
+            >
+              Log In Using Existing Account
+            </a>
+          </div>
+          <div
+            id="map"
+            className="w-[80%] lg:w-[800px] h-[400px] rounded border"
+          ></div>
+        </div>
       </div>
-      <div
-        id="map"
-        className="w-[80%] lg:w-[800px] h-[400px] rounded border"
-      ></div>
     </div>
   );
 }
