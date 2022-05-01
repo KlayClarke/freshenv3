c91d1cd98c5c27b6f2b5373fe29ee2c67f3f82fd
Klay Clarke <clarkeklay@gmail.com>
2022-05-01 15:52:13 -0400

    handle salon creation on api route (/api/salons/create)

---
+++ pages/api/salons/create.js
@@ -0,0 +1,52 @@
+import { getSession } from "next-auth/react";
+import prisma from "../../../lib/prisma";
+import findLocation from "../../../map/findLocation";
+import validator from "validator";
+
+export default async function handler(req, res) {
+  const session = await getSession({ req });
+  const { user_id } = session;
+
+  if (req.method === "POST") {
+    // Process POST request
+    const {
+      name,
+      type,
+      average_price,
+      image,
+      street_address,
+      city,
+      state,
+      zip_code,
+    } = req.body;
+
+    const coordinates =
+      (await findLocation(street_address, city, state, zip_code)) || [];
+
+    try {
+      await prisma.user.update({
+        where: {
+          id: user_id,
+        },
+        data: {
+          salons: {
+            create: {
+              name,
+              type,
+              average_price: parseInt(average_price),
+              image,
+              street_address,
+              city,
+              state,
+              zip_code,
+              coordinates,
+            },
+          },
+        },
+      });
+    } catch (err) {
+      console.log("Failure: ", err);
+    }
+    res.end();
+  }
+}
