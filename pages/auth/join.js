a1b190d0b0932748909bf5684776d002d376c442
Klay Clarke <clarkeklay@gmail.com>
2022-05-01 10:29:28 -0400

    remove form (no longer handling username, password auth), add github auth button, add page header on join and login routes

--- pages/auth/join.js
+++ pages/auth/join.js
@@ -1,120 +1,42 @@
-import { useState } from "react";
 import { signIn } from "next-auth/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { faGoogle, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function Join() {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-    salons: [],
-    reviews: [],
-  });
-
-  async function join(data) {
-    try {
-      fetch(process.env.NEXT_PUBLIC_USER_API_ENDPOINT + "/create", {
-        body: JSON.stringify(data),
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-      }).then(() => {});
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  async function handleSubmit(data) {
-    try {
-      join(data);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
-      <button
-        className="bg-white sm:shadow-sm sm:border sm:rounded-lg px-8 py-2 mb-4 flex items-center hover:bg-gray-50 border rounded"
-        onClick={() => {
-          signIn("google", {
-            callbackUrl: "/",
-          });
-        }}
-      >
-        <FontAwesomeIcon icon={faGoogle} className="mr-2 text-blue-500" />
-        <p className="text-blue-500 font-semibold">Join With Google</p>
-      </button>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit(formData);
-        }}
-        className="bg-white sm:shadow-sm sm:border sm:rounded-lg px-8 pt-6 pb-8 mb-4"
-      >
-        <div className="form-section">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="email"
-          >
-            Email
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500 focus:border-opacity-50"
-            id="email"
-            type={"email"}
-            onChange={(e) =>
-              setFormData({ ...formData, email: e.target.value })
-            }
-            value={formData.email}
-          />
-        </div>
-        <div className="form-section">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="username"
-          >
-            Username
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500 focus:border-opacity-50"
-            id="username"
-            type={"text"}
-            onChange={(e) =>
-              setFormData({ ...formData, username: e.target.value })
-            }
-            value={formData.username}
-          />
-        </div>
-        <div className="form-section">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="password"
-          >
-            Password
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500 focus:border-opacity-50"
-            id="password"
-            type={"password"}
-            onChange={(e) =>
-              setFormData({ ...formData, password: e.target.value })
-            }
-            value={formData.password}
-          />
-        </div>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 border border-blue-500 rounded w-full">
-          Join
+      <h1 className="text-5xl text-blue-500">Join</h1>
+      <br />
+      <div className="bg-white sm:shadow-sm sm:border sm:rounded-lg px-8 pt-6 pb-8 mb-4 flex flex-col justify-center items-center">
+        <button
+          className="bg-white sm:shadow-sm sm:border sm:rounded-lg px-8 py-2 mb-4 flex items-center hover:bg-gray-50 border rounded"
+          onClick={() => {
+            signIn("google", {
+              callbackUrl: "/",
+            });
+          }}
+        >
+          <FontAwesomeIcon icon={faGoogle} className="mr-2 text-blue-500" />
+          <p className="text-blue-500 font-semibold">Join With Google</p>
+        </button>
+        <button
+          className="bg-black sm:shadow-sm sm:border sm:rounded-lg px-8 py-2 mb-4 flex items-center hover:bg-gray-800 border rounded"
+          onClick={() => {
+            signIn("github", {
+              callbackUrl: "/",
+            });
+          }}
+        >
+          <FontAwesomeIcon icon={faGithub} className="mr-2 text-white" />
+          <p className="text-white font-semibold">Join With Github</p>
         </button>
         <p className="mt-4">
           Already have an account?{" "}
-          <a href="/auth/login" className="text-blue-500">
+          <a href="/auth/login" className="text-blue-500 font-semibold">
             Log In
           </a>{" "}
         </p>
-      </form>
+      </div>
     </div>
   );
 }
--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { getCsrfToken, signIn } from "next-auth/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { faGoogle, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function Login({ csrfToken }) {
   const [formData, setFormData] = useState({
@@ -15,77 +15,38 @@ export default function Login({ csrfToken }) {
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
-      <button
-        className="bg-white sm:shadow-sm sm:border sm:rounded-lg px-8 py-2 mb-4 flex items-center hover:bg-gray-50 border rounded"
-        onClick={() => {
-          signIn("google", {
-            callbackUrl: "/",
-          });
-        }}
-      >
-        <FontAwesomeIcon icon={faGoogle} className="mr-2 text-blue-500" />
-        <p className="text-blue-500 font-semibold">Login With Google</p>
-      </button>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          signIn("credentials", { username: "", password: "" });
-        }}
-        className="bg-white sm:shadow-sm sm:border sm:rounded-lg px-8 pt-6 pb-8 mb-4"
-      >
-        {/* <input name="csrfToken" type="hidden" defaultValue={csrfToken} /> */}
-        <div className="form-section">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="username"
-          >
-            Username
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500 focus:border-opacity-50"
-            id="username"
-            type={"text"}
-            onChange={(e) => {
-              setFormData({ ...formData, username: e.target.value });
-            }}
-            value={formData.username}
-          />
-        </div>
-        <div className="form-section">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="password"
-          >
-            Password
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500 focus:border-opacity-50"
-            id="password"
-            type={"password"}
-            onChange={(e) => {
-              setFormData({ ...formData, password: e.target.value });
-            }}
-            value={formData.password}
-          />
-        </div>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 border border-blue-500 rounded w-full">
-          Login
+      <h1 className="text-5xl text-blue-500">Login</h1>
+      <br />
+      <div className="bg-white sm:shadow-sm sm:border sm:rounded-lg px-8 pt-6 pb-8 mb-4 flex flex-col justify-center items-center">
+        <button
+          className="bg-white sm:shadow-sm sm:border sm:rounded-lg px-8 py-2 mb-4 flex items-center hover:bg-gray-50 border rounded"
+          onClick={() => {
+            signIn("google", {
+              callbackUrl: "/",
+            });
+          }}
+        >
+          <FontAwesomeIcon icon={faGoogle} className="mr-2 text-blue-500" />
+          <p className="text-blue-500 font-semibold">Login With Google</p>
+        </button>
+        <button
+          className="bg-black sm:shadow-sm sm:border sm:rounded-lg px-8 py-2 mb-4 flex items-center hover:bg-gray-800 border rounded"
+          onClick={() => {
+            signIn("github", {
+              callbackUrl: "/",
+            });
+          }}
+        >
+          <FontAwesomeIcon icon={faGithub} className="mr-2 text-white" />
+          <p className="text-white font-semibold">Login With Github</p>
         </button>
         <p className="mt-4">
           Don't have an account?{" "}
-          <a href="/auth/join" className="text-blue-500">
+          <a href="/auth/join" className="text-blue-500 font-semibold">
             Join
           </a>{" "}
         </p>
-      </form>
+      </div>
     </div>
   );
-}
-
-export async function getServerSideProps(context) {
-  return {
-    props: {
-      csrfToken: await getCsrfToken(context),
-    },
-  };
 }
