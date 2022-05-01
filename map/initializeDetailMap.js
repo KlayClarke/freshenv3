72adc115ddf0b9f12385b573f93030e11237337b
Klay Clarke <clarkeklay@gmail.com>
2022-05-01 16:27:30 -0400

    tweak map config to understand prisma data

--- map/initializeDetailMap.js
+++ map/initializeDetailMap.js
@@ -1,7 +1,7 @@
 export default function initializeDetailMap(mapboxgl, map, salon) {
   map.addControl(new mapboxgl.FullscreenControl());
   const marker1 = new mapboxgl.Marker()
-    .setLngLat(salon.geometry.coordinates)
+    .setLngLat(salon.coordinates)
     .setPopup(
       new mapboxgl.Popup({ offset: 25 }).setHTML(`<h2>${salon.name}</h2>`)
     )
