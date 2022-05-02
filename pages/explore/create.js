371843cfc15ab97d6bc139702498f20169c34fed
Klay Clarke <clarkeklay@gmail.com>
2022-05-02 08:43:09 -0400

    add select input for salon type

--- pages/explore/create.js
+++ pages/explore/create.js
@@ -77,15 +77,28 @@ export default function SalonCreate() {
           >
             Type
           </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500 focus:border-opacity-50"
-            placeholder="Type"
-            name="type"
-            onChange={(e) => {
-              setFormData({ ...formData, type: e.target.value });
-            }}
-            value={formData.type}
-          />
+          <div className="relative w-[203px]">
+            <select
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500 focus:border-opacity-50"
+              onChange={(e) =>
+                setFormData({ ...formData, type: e.target.value })
+              }
+              value={formData.type}
+            >
+              <option value={"barbershop"}>Barbershop</option>
+              <option value={"hybrid"}>Hybrid</option>
+              <option value={"salon"}>Salon</option>
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <svg
+                className="fill-current h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+              </svg>
+            </div>
+          </div>
         </div>
         <div className="form-section">
           <label
