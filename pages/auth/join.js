c6162a84f8328a2ed5e54e84420f976e0d656a43
Klay Clarke <clarkeklay@gmail.com>
2022-05-01 21:25:41 -0400

    change header text, change text in auth buttons

--- pages/auth/join.js
+++ pages/auth/join.js
@@ -5,7 +5,7 @@ import { faGoogle, faGithub } from "@fortawesome/free-brands-svg-icons";
 export default function Join() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
-      <h1 className="text-5xl text-blue-500 font-semibold">Join</h1>
+      <h1 className="text-5xl text-blue-500 font-semibold">Get Started</h1>
       <br />
       <div className="bg-white sm:shadow-sm sm:border sm:rounded-lg px-8 pt-6 pb-8 mb-4 flex flex-col justify-center items-center">
         <button
@@ -17,7 +17,7 @@ export default function Join() {
           }}
         >
           <FontAwesomeIcon icon={faGoogle} className="mr-2 text-black" />
-          <p className="text-black font-semibold">With Google</p>
+          <p className="text-black font-semibold">Login With Google</p>
         </button>
         <button
           className="bg-black sm:shadow-sm sm:border sm:rounded-lg px-8 py-2 mb-4 flex items-center hover:bg-gray-800 border rounded"
@@ -28,7 +28,7 @@ export default function Join() {
           }}
         >
           <FontAwesomeIcon icon={faGithub} className="mr-2 text-white" />
-          <p className="text-white font-semibold">With Github</p>
+          <p className="text-white font-semibold">Login With Github</p>
         </button>
         <p className="mt-4">
           Already have an account?{" "}
--- pages/auth/login.js
+++ pages/auth/login.js
@@ -15,7 +15,7 @@ export default function Login({ csrfToken }) {
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
-      <h1 className="text-5xl text-blue-500 font-semibold">Login</h1>
+      <h1 className="text-5xl text-blue-500 font-semibold">Welcome Back</h1>
       <br />
       <div className="bg-white sm:shadow-sm sm:border sm:rounded-lg px-8 pt-6 pb-8 mb-4 flex flex-col justify-center items-center">
         <button
@@ -27,7 +27,7 @@ export default function Login({ csrfToken }) {
           }}
         >
           <FontAwesomeIcon icon={faGoogle} className="mr-2 text-black" />
-          <p className="text-black font-semibold">With Google</p>
+          <p className="text-black font-semibold">Login With Google</p>
         </button>
         <button
           className="bg-black sm:shadow-sm sm:border sm:rounded-lg px-8 py-2 mb-4 flex items-center hover:bg-gray-800 border rounded"
@@ -38,7 +38,7 @@ export default function Login({ csrfToken }) {
           }}
         >
           <FontAwesomeIcon icon={faGithub} className="mr-2 text-white" />
-          <p className="text-white font-semibold">With Github</p>
+          <p className="text-white font-semibold">Login With Github</p>
         </button>
         <p className="mt-4">
           Don't have an account?{" "}
