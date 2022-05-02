6dce0ae2269a6fb6da2a109b4cb2d8ade67cbaa4
Klay Clarke <clarkeklay@gmail.com>
2022-05-02 08:43:19 -0400

    revise styles for select input on explore page

--- pages/explore/index.js
+++ pages/explore/index.js
@@ -28,7 +28,7 @@ export default function Explore({ salonsByName, salonsByType, salonsByPrice }) {
           <div className="relative w-64">
             <select
               onChange={(e) => setSortBy(e.target.value)}
-              className="block appearance-none w-full bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-500 focus:border-opacity-50"
             >
               <option value={"name"}>Sort by name</option>
               <option value={"type"}>Sort by type</option>
