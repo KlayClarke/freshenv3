dda1044ef9f7a9ffe8b969e2e35c628915d5d8e6
Klay Clarke <clarkeklay@gmail.com>
2022-05-01 21:24:58 -0400

    style home index page as a product landing page

--- pages/index.js
+++ pages/index.js
@@ -39,37 +39,104 @@ export default function Home({ salons }) {
   }, [pageIsMounted, data, Map]);
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="w-full xl:max-w-[1600px]">
-        <div className="flex flex-col justify-center items-center gap-10">
-          <Head>
-            <Script src="https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.js" />
-          </Head>
-          <h1 className="text-4xl max-w-4xl text-center text-blue-500">
-            Whether you are a customer searching for a new hairstylist or a
-            hairstylist searching for new customers,{" "}
-            <span className="font-bold">freshen</span> is the answer
-          </h1>
-          <div className="flex gap-5">
-            <a
-              href="/auth/join"
-              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 border border-blue-500 rounded"
-            >
-              Create New Account
-            </a>
-            <a
-              href="/auth/login"
-              className="bg-white hover:bg-gray-50 text-blue-500 font-bold py-2 px-4 border border-blue-500 rounded"
-            >
-              Log In Using Existing Account
-            </a>
+    <div>
+      <Head>
+        <Script src="https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.js" />
+      </Head>
+      {/* hero */}
+      <section className="relative">
+        <div className="container mx-auto flex flex-col-reverse lg:flex-row items-center gap-12 mt-14 lg:mt-28 px-10">
+          {/* Content */}
+          <div className="flex flex-1 flex-col items-center lg:items-start">
+            <h2 className="text-blue-500 text-3xl md:text-4xl lg:text-6xl text-center lg:text-left mb-6 font-semibold">
+              Welcome to freshen!
+            </h2>
+            <p className="text-gray-500 text-xl text-center lg:text-left mb-6">
+              Are you a hairstylist searching for new customers? Are you an
+              eager customer searching for a new look?
+            </p>
+            <div className="flex justify-center flex-wrap gap-6">
+              <a
+                href="/auth/join"
+                className="btn bg-blue-500 text-white font-semibold hover:bg-blue-600"
+              >
+                Get Started
+              </a>
+              <a
+                href="/auth/login"
+                className="btn bg-gray-50 text-black font-semibold hover:bg-gray-200"
+              >
+                Login with Existing Account
+              </a>
+            </div>
           </div>
-          <div
-            id="map"
-            className="w-[80%] lg:w-[800px] h-[400px] rounded border"
-          ></div>
         </div>
-      </div>
+      </section>
+      {/* features */}
+      <section className="bg-gray-50 py-20 mt-20 lg:mt-60">
+        {/* heading */}
+        <div className="max-w-[1000px] mx-auto px-2">
+          <h1 className="text-4xl text-center text-blue-500">Features</h1>
+          <p className="text-xl text-center text-gray-400 px-10">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin purus
+            nulla, rhoncus eget eleifend vel, vestibulum sed arcu. Curabitur
+            eleifend tristique metus eget dapibus. Proin eu ipsum sit amet eros
+            gravida placerat vel quis nibh. Nullam id fringilla turpis.
+            Suspendisse ullamcorper ligula non ex fringilla pulvinar. Nunc
+            maximus mi sed cursus porta. Sed ut accumsan arcu. Maecenas libero
+            dolor, hendrerit ut sodales eget, suscipit nec lacus. Morbi faucibus
+            quam et diam sollicitudin, non iaculis quam condimentum. In a nibh
+            faucibus quam ullamcorper molestie. Etiam ac maximus justo, eu
+            suscipit nisl. Donec vitae tellus dui. Fusce eu orci sit amet tortor
+            convallis auctor.
+          </p>
+        </div>
+        {/* feature 1 */}
+        <div className="relative mt-20 lg:mt-24 px-10">
+          <div className="container mx-auto flex flex-col lg:flex-row items-center justify-center gap-x-24">
+            {/* image */}
+
+            <div
+              id="map"
+              className="flex flex-1 justify-center mb-10 lg:mb-0 min-h-[400px] rounded-md border"
+            ></div>
+
+            {/* content */}
+            <div className="flex flex-1 flex-col items-center lg:items-start">
+              <h1 className="text-3xl text-blue-500">
+                Sample Text for feature 1...
+              </h1>
+              <p className="text-gray-400 my-4 text-center lg:text-left sm:w-3/4 lg:w-full"></p>
+              <a
+                href="/explore"
+                className="btn bg-blue-500 text-white font-semibold hover:bg-blue-600"
+              >
+                Explore
+              </a>
+            </div>
+          </div>
+        </div>
+        {/* feature 2 */}
+        <div className="relative mt-20 lg:mt-52 px-10">
+          <div className="container mx-auto flex flex-col lg:flex-row-reverse items-center justify-center gap-x-24">
+            {/* image */}
+            <div className="flex flex-1 justify-center z-10 mb-10 lg:mb-0"></div>
+            {/* content */}
+            <div className="flex flex-1 flex-col items-center lg:items-start">
+              <h1 className="text-3xl text-blue-500">
+                Sample Text for feature 2...
+              </h1>
+              <p className="text-gray-400 my-4 text-center lg:text-left sm:w-3/4 lg:w-full"></p>
+              <a
+                href="/explore"
+                className="btn bg-blue-500 text-white font-semibold hover:bg-blue-600"
+              >
+                Explore
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
