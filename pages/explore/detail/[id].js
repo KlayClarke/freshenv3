080fb6dd6ce76309444c1ecb44d76ba82363fb12
Klay Clarke <clarkeklay@gmail.com>
2022-05-02 23:10:30 -0400

    style salon detail demo page -- so that I can deploy something even whilst still constructing the app

--- pages/explore/detail/[id].js
+++ pages/explore/detail/[id].js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import mapboxgl from "mapbox-gl";
 import initializeDetailMap from "../../../map/initializeDetailMap";
 import prisma from "../../../lib/prisma";
+import Link from "next/link";
 
 // todo: render mapbox map on explore detail page
 
@@ -32,9 +33,39 @@ export default function Detail({ salon }) {
   }, [pageIsMounted, salon, Map]);
 
   return (
-    <div>
-      <h1 className="text-4xl">{salon.name}</h1>
-      <div id="map" className="w-[1000px] h-[400px]"></div>
+    <div className="flex items-center justify-center">
+      <div className="xl:min-w-[1400px] max-w-[1400px]">
+        {/* hero */}
+        <section className="relative">
+          <div id="map" className="h-[200px] lg:min-h-[300px]"></div>
+          <div className="container mx-auto flex flex-col-reverse lg:flex-row items-center mt-10 px-10"></div>
+        </section>
+        {/* features */}
+        <section className="bg-gray-50 py-20 mt-10 lg:mt-30">
+          {/* heading */}
+          <div className="max-w-[1000px] mx-auto px-2">
+            <h1 className="text-4xl text-center text-blue-500 font-semibold">
+              {salon.name}
+            </h1>
+            <br />
+
+            <h1 className="text-4xl text-center text-blue-500 font-semibold">
+              This page is under construction!
+            </h1>
+          </div>
+          {/* feature 1 */}
+          <div className="relative mt-20 lg:mt-24 px-10">
+            <div className="container mx-auto flex flex-col lg:flex-row items-center justify-center gap-x-24">
+              {/* content */}
+              <div className="flex flex-1 flex-col items-center">
+                <h1 className="text-4xl text-center text-blue-500 font-semibold">
+                  Comment section, private messaging, etc. coming soon.
+                </h1>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
