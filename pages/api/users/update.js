a149a67ff88506feae3674193954dff16daf66f3
Klay Clarke <clarkeklay@gmail.com>
2022-05-03 09:46:20 -0400

    create an update endpoint on the users api -- for now, we are only updating user zip code (zip code is optional, so if user has on already, we update it. On the contrary, if user does not have a zip code currently in the db, we store one)

---
+++ pages/api/users/update.js
@@ -0,0 +1,27 @@
+import { getSession } from "next-auth/react";
+import prisma from "../../../lib/prisma";
+
+export default async function handler(req, res) {
+  const session = await getSession({ req });
+  const { user_id } = session;
+
+  if (req.method === "PUT") {
+    // Process UPDATE request
+    const { zipCode } = req.body;
+    console.log(zipCode);
+
+    try {
+      await prisma.user.update({
+        where: {
+          id: user_id,
+        },
+        data: {
+          zipCode,
+        },
+      });
+    } catch (err) {
+      console.log("Failure: ", err);
+    }
+    res.end();
+  }
+}
