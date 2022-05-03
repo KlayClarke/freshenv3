575e82176b7b5e76311aa2484625e3a8e3375430
Klay Clarke <clarkeklay@gmail.com>
2022-05-03 09:44:49 -0400

    add zip code to session callback -- will allow us to access user zip from the session

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -27,6 +27,7 @@ export default NextAuth({
   callbacks: {
     session: async ({ session, user }) => {
       session.user_id = user.id;
+      session.zip_code = user.zipCode;
       return Promise.resolve(session);
     },
   },
