d42a09904a8283d4ac5cd1fb908d3bcb2e088d68
Klay Clarke <clarkeklay@gmail.com>
2022-05-07 00:28:49 -0400

    handle salon edit backend (on api)

---
+++ pages/api/salons/edit/[id].js
@@ -0,0 +1,56 @@
+import { getSession } from "next-auth/react";
+import prisma from "../../../../lib/prisma";
+import findLocation from "../../../../map/findLocation";
+
+export default async function handler(req, res) {
+  const session = await getSession({ req });
+  const { user_id } = session;
+  const { id } = req.query;
+
+  let salon = await prisma.salon.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (req.method === "POST" && session && user_id === salon.author_id) {
+    const {
+      name,
+      type,
+      average_price,
+      image,
+      street_address,
+      city,
+      state,
+      zip_code,
+    } = req.body;
+
+    const coordinates =
+      (await findLocation(street_address, city, state, zip_code)) || [];
+
+    try {
+      const salon = await prisma.salon.update({
+        where: {
+          id,
+        },
+        data: {
+          name,
+          type,
+          average_price: parseInt(average_price),
+          image,
+          street_address,
+          city,
+          state,
+          zip_code,
+          coordinates,
+        },
+      });
+      console.log("Update successful");
+    } catch (err) {
+      console.log("Failure: ", err);
+    }
+  } else {
+    res.status(401).json({ response: "401: Unauthorized request" });
+  }
+  res.end();
+}
