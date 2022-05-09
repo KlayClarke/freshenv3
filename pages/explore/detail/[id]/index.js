d58ff9f557995fecd4815557d96bb460f756d65b
Klay Clarke <clarkeklay@gmail.com>
2022-05-09 08:29:44 -0400

    refactor

--- pages/explore/detail/[id]/index.js
+++ pages/explore/detail/[id]/index.js
@@ -9,15 +9,12 @@ import sanitizeHtml from "sanitize-html";
 import unentity from "../../../../utils/unentity";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import Image from "next/image";
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 
 export default function Detail({ salon }) {
   const [pageIsMounted, setPageIsMounted] = useState(false);
   const [Map, setMap] = useState();
-  const [rating, setRating] = useState(3);
-  const [body, setBody] = useState("");
   const { data: session, status } = useSession();
   const { data: salons, error } = useSWR(
     process.env.NEXT_PUBLIC_SITE_ENDPOINT + "/api/salons/get",
@@ -25,37 +22,6 @@ export default function Detail({ salon }) {
   );
   const router = useRouter();
   const { id: salon_id } = router.query;
-
-  async function handleReviewCreation(e) {
-    e.preventDefault();
-    const res = await fetch(
-      process.env.NEXT_PUBLIC_SITE_ENDPOINT + "/api/reviews/create",
-      {
-        body: JSON.stringify({
-          rating,
-          body,
-          salon_id,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-      }
-    );
-    setBody("");
-    router.push(`/explore/detail/${salon_id}`);
-  }
-
-  async function handleReviewDeletion(e, id) {
-    e.preventDefault();
-    const res = await fetch(
-      process.env.NEXT_PUBLIC_SITE_ENDPOINT + `/api/reviews/delete/${id}`,
-      {
-        method: "POST",
-      }
-    );
-    router.push(`/explore/detail/${salon_id}`);
-  }
 
   useEffect(() => {
     setPageIsMounted(true);
@@ -68,11 +34,7 @@ export default function Detail({ salon }) {
     });
 
     setMap(map);
-<<<<<<< HEAD
   }, []);
-=======
-  }, [salon]);
->>>>>>> 7837de6806467746d659111f1dd83d564939949b
 
   useEffect(() => {
     if (pageIsMounted && salons) {
@@ -80,11 +42,7 @@ export default function Detail({ salon }) {
         initializeClusterMap(mapboxgl, Map, salons);
       });
     }
-<<<<<<< HEAD
   }, [pageIsMounted, salons, Map]);
-=======
-  }, [pageIsMounted]);
->>>>>>> 7837de6806467746d659111f1dd83d564939949b
 
   return (
     <div className="flex items-center justify-center">
