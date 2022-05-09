64c5d60b9041c622917c66284e4f1be6a322735d
Klay Clarke <clarkeklay@gmail.com>
2022-05-08 22:09:03 -0400

    revert back to getServerSideProps

--- pages/explore/detail/[id]/index.js
+++ pages/explore/detail/[id]/index.js
@@ -276,7 +276,7 @@ export default function Detail({ salon, reviews }) {
   );
 }
 
-export async function getInitialProps({ query }) {
+export async function getServerSideProps({ query }) {
   const salon = await prisma.salon.findUnique({
     where: {
       id: query.id,
