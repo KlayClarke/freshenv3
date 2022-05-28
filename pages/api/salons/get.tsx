6dec6d13c6ff6dc2b6f5e91d0a5d7ca0ecf81ad7
Klay Clarke <clarkeklay@gmail.com>
2022-05-27 23:10:43 -0400

    uncomment salons api code and comment out nextjs cors demo code (pushed app with demo code enabled to test whether it really bypasses cors errors or not -- it does)

--- pages/api/salons/get.tsx
+++ pages/api/salons/get.tsx
@@ -14,57 +14,52 @@ const cors = initMiddleware(
   })
 );
 
-export default async function handler(req, res) {
-  // Run cors
+// export default async function handler(req, res) {
+//   // Run cors
+//   await cors(req, res);
+
+//   // Rest of the API logic
+//   res.json({ message: "Hello Everyone!" });
+// }
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  // run cors
   await cors(req, res);
 
-  // Rest of the API logic
-  res.json({ message: "Hello Everyone!" });
+  const salons = (await prisma.salon.findMany({})) as Salon[];
+
+  const formattedData: Salon[] = [];
+
+  salons.map((salon) => {
+    const formattedSalon = {} as Salon;
+
+    formattedSalon.geometry = {
+      type: "Point",
+      coordinates: salon.coordinates,
+    };
+    formattedSalon.id = salon.id;
+    formattedSalon.name = salon.name;
+    formattedSalon.type = salon.type;
+    formattedSalon.average_price = salon.average_price;
+    formattedSalon.image = salon.image;
+    formattedSalon.street_address = salon.street_address;
+    formattedSalon.city = salon.city;
+    formattedSalon.state = salon.state;
+    formattedSalon.zip_code = salon.zip_code;
+    formattedSalon.properties = {
+      mapboxClusterHTML:
+        "<h3 class='text-center'><a href=" +
+        `/explore/detail/${salon.id}` +
+        " class='text-2xl text-blue-500 underline'>" +
+        salon.name +
+        "</a></h3>",
+    };
+
+    formattedData.push(formattedSalon);
+  });
+
+  res.json({ features: formattedData });
 }
-
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse<any>
-// ) {
-//   await NextCors(req, res, {
-//     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-//     origin: "*",
-//     optionsSuccessStatus: 200,
-//   });
-
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-
-//   const salons = (await prisma.salon.findMany({})) as Salon[];
-
-//   const formattedData: Salon[] = [];
-
-//   salons.map((salon) => {
-//     const formattedSalon = {} as Salon;
-
-//     formattedSalon.geometry = {
-//       type: "Point",
-//       coordinates: salon.coordinates,
-//     };
-//     formattedSalon.id = salon.id;
-//     formattedSalon.name = salon.name;
-//     formattedSalon.type = salon.type;
-//     formattedSalon.average_price = salon.average_price;
-//     formattedSalon.image = salon.image;
-//     formattedSalon.street_address = salon.street_address;
-//     formattedSalon.city = salon.city;
-//     formattedSalon.state = salon.state;
-//     formattedSalon.zip_code = salon.zip_code;
-//     formattedSalon.properties = {
-//       mapboxClusterHTML:
-//         "<h3 class='text-center'><a href=" +
-//         `/explore/detail/${salon.id}` +
-//         " class='text-2xl text-blue-500 underline'>" +
-//         salon.name +
-//         "</a></h3>",
-//     };
-
-//     formattedData.push(formattedSalon);
-//   });
-
-//   res.json({ features: formattedData });
-// }
