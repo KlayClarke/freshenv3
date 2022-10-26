7b1fca3fc71b5dcff372acdfaeffc6d205cec701
Klay Clarke <clarkeklay@gmail.com>
2022-10-25 20:38:50 -0400

    create empty booking form landing page - user is directed here after pressing appointment book button on detail page

---
+++ components/Salons/BookingForm.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Salon } from "../../atoms/salonsAtom";
+
+type BookingFormProps = {
+  salon: Salon;
+};
+
+const BookingForm: React.FC<BookingFormProps> = ({ salon }) => {
+  return (
+    <div>
+      <></>
+    </div>
+  );
+};
+
+export default BookingForm;
