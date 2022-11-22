b3d2a64d73f0933017f859566bea175c5e86d4e3
Klay Clarke <clarkeklay@gmail.com>
2022-11-22 12:51:09 -0500

    misc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html>
       <Head>
         <link
-          href="https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.css"
+          href="https://api.mapbox.com/mapbox-gl-js/v2.11.0/mapbox-gl.css"
           rel="stylesheet"
         />
       </Head>
       <body>
         <Main />
+        <Script src="https://api.mapbox.com/mapbox-gl-js/v2.11.0/mapbox-gl.js" />
         <NextScript />
       </body>
     </Html>
