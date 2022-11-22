ed4e603ae0609ea66d9411cb5b68a6d4cd3734c4
Klay Clarke <clarkeklay@gmail.com>
2022-11-22 18:13:32 -0500

    remove next/script from _document file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
@@ -12,7 +11,6 @@ export default function Document() {
       </Head>
       <body>
         <Main />
-        <Script src="https://api.mapbox.com/mapbox-gl-js/v2.11.0/mapbox-gl.js" />
         <NextScript />
       </body>
     </Html>
